939c9870c2d9d4add6cffc8d9ceb045cc2663b9c
Obsidian Astralis <145642148+ObsidianAstralis@users.noreply.github.com>
2025-04-09 04:28:50 +0000

    Added a p tag within the flask.

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def home():
-    return "<h1>Obsidian's Flask</h1>"
+    # return "<h1>Obsidian's Flask</h1>"
+    return """
+    <h1>Obsidian's Flask</h1>
+    <p>This is a paragraph tag inside the Flask</p>
+"""
 
 if __name__ == "__main__":
     app.run()
